f800dda6e50eeca9a3a1bdb7f95ab706d0d5c137
shyam sundar <shyamsundar055@gmail.com>
2020-03-26 15:56:16 +0530

    Updates

---
+++ precache-manifest.2c10b7cf8d43b5f453f3968f2df24241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b695ac40229f955980a02636151533",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "3ca4158f8e79c9307ff6",
+    "url": "/Todo-App/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ec4534d13b0899f351c3",
+    "url": "/Todo-App/static/js/2.5fe802d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App/static/js/2.5fe802d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca4158f8e79c9307ff6",
+    "url": "/Todo-App/static/js/main.0a870126.chunk.js"
+  },
+  {
+    "revision": "867d6b47fb0f77f3b9eb",
+    "url": "/Todo-App/static/js/runtime-main.be44b1c7.js"
+  }
+]);
